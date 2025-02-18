bad82f9eff0dc9f19d9537744f941aa990323ab6
gaviotanacolher <luizagrudka@gmail.com>
2025-02-17 21:13:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click',function() {
+    document.body.classList.toggle('dark-theme')
+
+ var className=document.body.className;
+ if(className =="light-theme") {
+    console.log('current class name:' + className);
+    this.textContent = "Dark";
+ }
+ else {
+    this.textContent = "Light";
+ }
+});
+
